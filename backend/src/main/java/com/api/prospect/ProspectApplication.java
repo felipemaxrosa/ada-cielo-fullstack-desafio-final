b0f4e62da54ca9a029d4732deea1f0a30c193e04
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-09-25 23:48:59 +0100

    feat: backend start config

---
+++ backend/src/main/java/com/api/prospect/ProspectApplication.java
@@ -0,0 +1,13 @@
+package com.api.prospect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspectApplication.class, args);
+	}
+
+}
