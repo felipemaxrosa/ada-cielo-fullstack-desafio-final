ab73aa443d0583a8e4695521d7bd70f8a29352e8
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-09-26 00:11:18 +0100

    feat: pessoa fisica model

--- backend/src/main/java/com/api/prospect/models/PessoaFisicaModel.java
+++ backend/src/main/java/com/api/prospect/models/PessoaFisicaModel.java
@@ -1,2 +1,19 @@
-package com.api.prospect.models;public class PessoaFisicaModel {
+package com.api.prospect.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="TB_PROSPECT_PF")
+@Setter
+@Getter
+public class PessoaFisicaModel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String cpf;
+  private String mcc;
+  private String contactName;
+  private String contactEmail;
 }
