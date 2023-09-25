4ae9cd0c0f8ee68d3bd3ab421961744aaa2d4ecc
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-09-26 00:10:55 +0100

    feat: pessoa juridica model

---
+++ backend/src/main/java/com/api/prospect/models/PessoaFisicaModel.java
@@ -0,0 +1,2 @@
+package com.api.prospect.models;public class PessoaFisicaModel {
+}
---
+++ backend/src/main/java/com/api/prospect/models/PessoaJuridicaModel.java
@@ -0,0 +1,20 @@
+package com.api.prospect.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="TB_PROSPECT_PJ")
+@Setter
+@Getter
+public class PessoaJuridicaModel {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String cnpj;
+  private String corporateName;
+  private String mcc;
+  private String contactName;
+  private String contactEmail;
+}
