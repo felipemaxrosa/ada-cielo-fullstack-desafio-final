d688d944d304616f2da2539a241ec4aa1109486a
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-09-26 22:33:50 +0100

    feat: pessoa fisica validation

--- backend/src/main/java/com/api/prospect/controllers/PessoaFisicaController.java
+++ backend/src/main/java/com/api/prospect/controllers/PessoaFisicaController.java
@@ -8,7 +8,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -27,5 +32,19 @@ public class PessoaFisicaController {
     BeanUtils.copyProperties(pessoaFisicaDto, pessoaFisicaModel);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(pessoaFisicaRepository.save(pessoaFisicaModel));
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+      String fieldName = ((FieldError) error).getField();
+      String errorMessage = error.getDefaultMessage();
+      errors.put(fieldName, errorMessage);
+    });
+
+    return errors;
   }
 }
