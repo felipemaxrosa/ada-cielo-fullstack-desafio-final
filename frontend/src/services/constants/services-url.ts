ef8803973853c951a363de42496f7de5a0ecf4f2
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-09-27 23:18:59 +0100

    feat: start config

---
+++ frontend/src/services/constants/services-url.ts
@@ -0,0 +1,17 @@
+export const API_URL = 'http://localhost:8080/';
+export const FEEDBACK = '/feedback';
+export const FEEDBACK_ELOGIO = '/feedback/elogio';
+export const FEEDBACK_SUGESTAO = '/feedback/sugestao';
+export const FEEDBACK_CRITICA = '/feedback/critica';
+
+export const PESSOA_FISICA = {
+  BASE_URL: '/prospect-pessoa-fisica',
+  QUEUE: '/prospect-pessoa-fisica/service-queue',
+  NEXT_PROSPECT: '/prospect-pessoa-fisica/service-queue/next-prospect',
+};
+
+export const PESSOA_JURIDICA = {
+  BASE_URL: '/prospect-pessoa-juridica',
+  QUEUE: '/prospect-pessoa-juridica/service-queue',
+  NEXT_PROSPECT: '/prospect-pessoa-juridica/service-queue/next-prospect',
+};
