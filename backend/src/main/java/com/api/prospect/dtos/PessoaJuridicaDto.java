b56a25b1c40f60dd5c2e519bcf08ea1e7184be87
Felipe Rosa <felipemax.suporteti@gmail.com>
2023-09-29 19:13:12 +0100

    refactor: errors returning

--- backend/src/main/java/com/api/prospect/dtos/PessoaJuridicaDto.java
+++ backend/src/main/java/com/api/prospect/dtos/PessoaJuridicaDto.java
@@ -9,31 +9,31 @@ import lombok.Setter;
 public class PessoaJuridicaDto {
   @Size(max = 14, message = "CNPJ must have at maximum, 14 characters")
   @NotBlank(message = "CNPJ is required")
-  @NotNull(message = "CNPJ can not be null")
+  @NotNull(message = "CNPJ is required")
   private String cnpj;
 
   @Size(max = 50, message = "Corporate name must have at maximum, 50 characters")
   @NotBlank(message = "Corporate name is required")
-  @NotNull(message = "Corporate name can not be null")
+  @NotNull(message = "Corporate name is required")
   private String corporateName;
 
   @Size(max = 4, message = "MCC must have at maximum, 4 characters")
   @NotBlank(message = "MCC (Merchant Category Code) is required")
-  @NotNull(message = "MCC (Merchant Category Code) can not be null")
+  @NotNull(message = "MCC (Merchant Category Code) is required")
   private String mcc;
 
   @Size(max = 11, message = "Contact CPF must have at maximum, 11 characters")
   @NotBlank(message = "Contact CPF is required")
-  @NotNull(message = "Contact CPF can not be null")
+  @NotNull(message = "Contact CPF is required")
   private String contactCpf;
 
   @Size(max = 50, message = "Contact name must have at maximum, 50 characters")
   @NotBlank(message = "Contact name is required")
-  @NotNull(message = "Contact can not be null")
+  @NotNull(message = "Contact name is required")
   private String contactName;
 
   @NotBlank(message = "Email is required")
-  @NotNull(message = "Email can not be null")
+  @NotNull(message = "Email is required")
   @Pattern(regexp = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$", message = "Invalid email format")
   private String contactEmail;
 }
